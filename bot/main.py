ce7e06739a859a3378fe9ff74aa0a6d6c3b876ac
qwerty <balezino2017@gmail.com>
2025-10-09 21:32:24 +0500

    Добавил команды бота и безопасно иницилизировал с логированием

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,10 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommandScopeDefault
 from bot.config.settings import settings
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from bot.core.bot_commands import get_bot_commands
 from bot.handlers.admin import admin
 from bot.utils.logger import logger
 from bot.handlers.user import message
@@ -15,6 +17,16 @@ async def on_startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     logger.info("Database initialized")
+    
+async def setup_bot_commands(bot: Bot):
+    # Устанавливает команды бота в Telegram.
+    # Безопасная инициализация с логированием.
+    try:
+        commands = get_bot_commands()
+        await bot.set_my_commands(commands, scope=BotCommandScopeDefault(), language_code='ru')
+        logger.success("Команды бота успешно установлены")
+    except Exception as e:
+        logger.error(f"Ошибка при установке команд бота: {e}")
 
 async def main():
     # Создаём экземпляр бота
@@ -26,6 +38,8 @@ async def main():
     
     # Инициализация базы данных
     await init_db()
+    
+    await setup_bot_commands(bot)
 
     await on_startup()
     logger.info("Bot started!")
