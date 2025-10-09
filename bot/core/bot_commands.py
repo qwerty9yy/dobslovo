3aba71b13ea8cd5186d07378694f3b83ac01ef0c
qwerty <balezino2017@gmail.com>
2025-10-09 21:34:04 +0500

    Создал папку для бот команд

---
+++ bot/core/bot_commands.py
@@ -0,0 +1,10 @@
+from aiogram.types import BotCommand
+
+def get_bot_commands() -> list[BotCommand]:
+    """Возвращает список команд бота."""
+    return [
+        BotCommand(command='/start', description='Главное меню'),
+        BotCommand(command='/contacts', description='Контакты'),
+        BotCommand(command='/about', description='О нас'),
+        BotCommand(command='/donate', description='Поддержать редакцию')
+    ]
