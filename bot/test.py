264f1427bb45f3078c3a6949fe64b9e52b008076
qwerty <balezino2017@gmail.com>
2025-10-11 22:24:56 +0500

    Тестовый файл

--- bot/test.py
+++ bot/test.py
@@ -2,16 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 # 2. Проверяем статус ответа
-url = 'https://dobslovo.ru/kupit-gazetu/'
+url = 'https://dobslovo.ru/'
 response = requests.get(url)
 
 # 2. Проверяем статус ответа
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    blog_items = soup.find_all('div', class_='elementor-toggle-item')
-    # print(blog_items)
-    for blog in blog_items:
-        name = blog.find('a', class_='elementor-toggle-title').text.strip()
-        text = blog.find('div', class_='elementor-tab-content').text.strip()
-        print(name, '----', text)
+    test = soup.find('span', class_='elementor-counter-number')
+    test = test.get('data-to-value')
+    print(test)
+    
+    # blog_items = soup.find_all('div', class_='elementor-toggle-item')
+    # # print(blog_items)
+    # for blog in blog_items:
+    #     name = blog.find('a', class_='elementor-toggle-title').text.strip()
+    #     text = blog.find('div', class_='elementor-tab-content').text.strip()
+    #     print(name, '----', text)
