8f692454e4d596a1474895fe7471951b636432f6
qwerty <balezino2017@gmail.com>
2025-10-16 22:02:57 +0500

    Клавиатура зайдайте вопрос(не используется)

---
+++ bot/keyboards/user/support.py
@@ -0,0 +1,10 @@
+# from aiogram.utils.keyboard import InlineKeyboardBuilder
+# from functools import lru_cache
+
+# @lru_cache
+# def get_menu_support():
+#     ''' Клавиатура меню "Задайте вопрос" Telegram-бота «Доброе Слово». '''
+#     builder = InlineKeyboardBuilder()
+#     builder.button(text='❌ Отменить', callback_data='cancel_support')
+#     builder.adjust(1)
+#     return builder.as_markup()
