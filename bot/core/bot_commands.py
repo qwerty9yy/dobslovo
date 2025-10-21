d927897f328ed05e102fe37c081a86b6b0a287cb
qwerty <balezino2017@gmail.com>
2025-10-21 22:39:40 +0500

    Поменял расположение командн

--- bot/core/bot_commands.py
+++ bot/core/bot_commands.py
@@ -4,9 +4,10 @@ def get_bot_commands() -> list[BotCommand]:
     """Возвращает список команд бота."""
     return [
         BotCommand(command='/start', description='Главное меню'),
-        BotCommand(command='/contacts', description='Контакты'),
-        BotCommand(command='/about', description='О нас'),
-        BotCommand(command='/donate', description='Поддержать редакцию'),
+        BotCommand(command='/newspaper', description='Газета'),
         BotCommand(command='/products', description='Продукция'),
-        BotCommand(command='/newspaper', description='Газета')
+        BotCommand(command='/donate', description='Поддержать редакцию'),
+        BotCommand(command='/bible', description='Библия'),
+        BotCommand(command='/contacts', description='Контакты'),
+        BotCommand(command='/about', description='О нас')
     ]
