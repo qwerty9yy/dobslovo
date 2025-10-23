55cb57fa3ca137139a99e46156bbcf6718a8bba6
qwerty <balezino2017@gmail.com>
2025-10-23 22:28:26 +0500

    Добавил клавиатуру добавь на сайт

--- bot/keyboards/user/keyboards.py
+++ bot/keyboards/user/keyboards.py
@@ -1,7 +1,10 @@
 from functools import lru_cache
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-support = 'https://t.me/qwert9yy'
+from bot.parsers.archives_parser import parse_archives_page
+
+support = 'https://t.me/sergey_kocsheev'
+URL = "https://dobslovo.ru"
 
 @lru_cache
 def get_contacts_menu():
@@ -12,9 +15,10 @@ def get_contacts_menu():
     builder.button(text='💬 Telegram', url='https://t.me/dobslovo')
     builder.button(text='💚 WhatsApp', url='https://chat.whatsapp.com/H286SE95wcTAUjeAG8JhMw')
     builder.button(text='💜 Viber', url='https://invite.viber.com/?g=Nn3Sl2xHHUT-_wcum_56Q5YO1VXFKmsA&lang=ru')
+    builder.button(text="🌐 Перейти на сайт", url=URL)
     builder.button(text='💬 Есть вопрос?', url=support)
     builder.button(text="🔙 Главное меню", callback_data="back_to_main")
-    builder.adjust(2, 2, 1, 1, 1)
+    builder.adjust(2, 2, 1, 1, 2)
     return builder.as_markup()
 
 
@@ -23,6 +27,7 @@ def get_menu_about_us():
     ''' Клавиатура меню "О нас" Telegram-бота «Доброе Слово». '''
     builder = InlineKeyboardBuilder()
     builder.button(text='💸 Поддержать', callback_data='donate')
+    builder.button(text="🌐 Перейти на сайт", url=f"{URL}/podderzhat-gazetu/")
     builder.button(text="🔙 Главное меню", callback_data="back_to_main")
     builder.adjust(1)
     return builder.as_markup()
@@ -33,48 +38,56 @@ def get_support_us():
     ''' Клавиатура меню "Поддержать" Telegram-бота «Доброе Слово». '''
     buider = InlineKeyboardBuilder()
     buider.button(text='🏦 Сбербанк', callback_data='bank_sberbank')
-    buider.button(text='💳 МТС Банк', callback_data='bank_mtsbank')
+    buider.button(text='💳 МТС', callback_data='bank_mtsbank')
     buider.button(text='📮 Почта России', callback_data='bank_mailrussia')
+    buider.button(text="🌐 Перейти на сайт", url=f"{URL}/podderzhat-gazetu/")
     buider.button(text='← О нас', callback_data='menu_about_us')
     buider.button(text="🔙 Главное меню", callback_data="back_to_main")
-    buider.adjust(1, 1, 1, 2)
+    buider.adjust(1, 1, 1, 1, 2)
     return buider.as_markup()
 
 @lru_cache
 def get_show_bank():
-   ''' Клавиатура меню "Выбора банка" Telegram-бота «Доброе Слово». '''
-   builder = InlineKeyboardBuilder()
-   builder.button(text='💬 Есть вопрос?', url=support)
-   builder.button(text='← Назад', callback_data='donate')
-   builder.button(text='🔙 Главное меню', callback_data='back_to_main')
-   builder.adjust(1, 2)
-   return builder.as_markup()
+    ''' Клавиатура меню "Выбора банка" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='💬 Есть вопрос?', url=support)
+    builder.button(text='← Назад', callback_data='donate')
+    builder.button(text='🔙 Главное меню', callback_data='back_to_main')
+    builder.adjust(1, 2)
+    return builder.as_markup()
 
 @lru_cache
 def get_menu_newspaper():
-   ''' Клавиатура меню "Газета" Telegram-бота «Доброе Слово». '''
-   builder = InlineKeyboardBuilder()
-   builder.button(text='🔙 Главное меню', callback_data='back_to_main')
-   builder.adjust(2)
-   return builder.as_markup()
-
-@lru_cache
-def get_menu_newspaper_search():
-   ''' Клавиатура меню "Поиск по архиву" Telegram-бота «Доброе Слово». '''
-   builder = InlineKeyboardBuilder()
-   builder.button(text='🔙 Главное меню', callback_data='back_to_main')
-   builder.adjust(1)
-   return builder.as_markup()
-
-def create_year_papers_keyboard(papers: list):
-    """Создает клавиатуру с газетами выбранного года"""
+    ''' Клавиатура меню "Газета" Telegram-бота «Доброе Слово». '''
     builder = InlineKeyboardBuilder()
-    
-    for paper in papers:
-        builder.button(
-            text=f"{paper['title']}",
-            callback_data=f"newspaper_{paper['year']}_{paper['issue']}"
-        )
+    builder.button(text="🌐 Перейти на сайт", url=f"{URL}/arhivy-gazety/")
     builder.button(text='🔙 Главное меню', callback_data='back_to_main')
     builder.adjust(2)
     return builder.as_markup()
+
+def get_menu_newspaper_search(newspapers):
+    ''' Клавиатура меню "Поиск по архиву" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    for newspaper in newspapers:
+        # Форматируем текст кнопки красивее
+        title_clean = newspaper['title'].replace('Газета ', '').replace('-', ' ')
+        builder.button(
+            text=f"📰 {title_clean}",
+            callback_data=f"newspaper_{newspaper['year']}_{newspaper['issue']}"
+        )
+    builder.button(text="🌐 Перейти на сайт", url=f"{URL}/arhivy-gazety/")
+    builder.button(text='🔙 Главное меню', callback_data='back_to_main')
+    builder.adjust(2)
+    return builder.as_markup()
+
+# def create_year_papers_keyboard(papers: list):
+#     """Создает клавиатуру с газетами выбранного года"""
+#     builder = InlineKeyboardBuilder()
+#     for paper in papers:
+#         builder.button(
+#             text=f"{paper['title']}",
+#             callback_data=f"newspaper_{paper['year']}_{paper['issue']}"
+#         )
+#     builder.button(text='🔙 Главное меню', callback_data='back_to_main')
+#     builder.adjust(2)
+#     return builder.as_markup()
