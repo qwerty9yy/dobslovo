0e980982f95393c5adcbb5926febdb33b0dc0fb7
qwerty <balezino2017@gmail.com>
2025-10-25 13:08:34 +0500

    Добавил реализацию замены изображение с помощью админки

--- bot/keyboards/admin/keyboard.py
+++ bot/keyboards/admin/keyboard.py
@@ -7,7 +7,9 @@ def get_admin_start_menu():
     builder = InlineKeyboardBuilder()
     builder.button(text='Статистика', callback_data='statistics')
     builder.button(text='Рассылка', callback_data='mailing')
-    builder.adjust(2)
+    builder.button(text='Товары', callback_data='admin_products')
+    builder.button(text='Газеты', callback_data='edit_newspapers_photo')
+    builder.adjust(1, 1, 2)
     return builder.as_markup()
 
 @lru_cache
@@ -33,4 +35,50 @@ def get_newsletter_step():
     builder.button(text='✅ Начать рассылку', callback_data='start_newsletter')
     builder.button(text='❌ Отменить', callback_data='cancel_newsletter')
     builder.adjust(2)
+    return builder.as_markup()
+
+@lru_cache
+def get_admin_products():
+    ''' Клавиатура меню "Календари Admin" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='Добавить', callback_data='add_calendars')
+    builder.button(text='Изменить', callback_data='replace_products')
+    builder.button(text='❌ Отменить', callback_data='back_to_admin')
+    builder.adjust(2, 1)
+    return builder.as_markup()
+
+@lru_cache
+def get_add_calendars():
+    ''' Клавиатура меню "Календари Добавить или Изменить Admin" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='❌ Отменить', callback_data='cancel_calendars')
+    builder.adjust(1)
+    return builder.as_markup()
+    
+@lru_cache
+def get_yes_no_change_calendars():
+    ''' Клавиатура меню "Подтверждение на удаление и добавлени изображений" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='Приступить', callback_data='get_started_change_calendars')
+    builder.button(text='❌ Отменить', callback_data='cancel_calendars')
+    builder.adjust(1)
+    return builder.as_markup()
+
+@lru_cache
+def get_edit_newspapers_photo():
+    ''' Клавиатура меню "Газета Admin" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='Добавить', callback_data='add_newspapers_photo')
+    builder.button(text='Изменить', callback_data='replace_newspapers_photo')
+    builder.button(text='❌ Отменить', callback_data='back_to_admin')
+    builder.adjust(2, 1)
+    return builder.as_markup()
+
+@lru_cache
+def get_yes_no_replace_newspapers_photo():
+    ''' Клавиатура меню "Подтверждение на удаление и добавлени изображений" Telegram-бота «Доброе Слово». '''
+    builder = InlineKeyboardBuilder()
+    builder.button(text='Приступить', callback_data='started_replace_newspapers_photo')
+    builder.button(text='❌ Отменить', callback_data='cancel_calendars')
+    builder.adjust(1)
     return builder.as_markup()
