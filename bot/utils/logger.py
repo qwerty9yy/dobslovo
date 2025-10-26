ca0ee6edb4bb5b152fb77ea6915a6a6a66a5d067
qwerty <balezino2017@gmail.com>
2025-10-26 18:34:19 +0500

    Добавил логирование в файл

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -5,6 +5,16 @@ import sys
 logger.remove()  # Удаляем стандартный обработчик
 logger.add(sys.stdout, format="<green>{time:HH:mm:ss}</green> | <level>{level}</level> | {message}", level="INFO")
 
+# Логи в файл (самый простой способ)
+logger.add(
+    "bot/logs/bot.log",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+    level="INFO",
+    rotation="10 MB",  # Ротация при достижении 10 МБ
+    retention="30 days",  # Хранить логи 30 дней
+    compression="zip"  # Сжимать старые логи
+)
+
 __all_ = ["logger"] # Экспортируем логгер для использования в других модулях
 
 # logging.basicConfig(
